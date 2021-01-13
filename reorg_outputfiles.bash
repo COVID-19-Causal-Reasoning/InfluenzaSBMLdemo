575d1075c2627ccac29b542e9ae91951102efee3
Jeremy Zucker <djinnome@gmail.com>
2021-01-13 11:33:44 -0800

    New networks for VN1203 vs Mock with kegg_influenza_ns1 input

--- reorg_outputfiles.bash
+++ reorg_outputfiles.bash
@@ -1,23 +1,17 @@
 VARIABLE=VN1203
 STRAIN="Mock NS1"
-BASELINE=NS1
-TIME="0h 3h 7h 12h 18h 24h"
-
+BASELINE=Mock
+TIME="0h  12h 18h 24h"
+PERTURBATION=kegg_influenza_ns1
 for BASELINE in $STRAIN
 do
-    if [ $BASELINE = "NS1" ]
-    then
-	PERTURBATION=HostVirus
-    else
-	PERTURBATION=NoInput
-    fi
 
     for CONSTANT in $TIME
     do
-	INPUTDIR=Results/${VARIABLE}vs${BASELINE}_for_${CONSTANT}r
-	#git mv $INPUTDIR/network_solution.dot Results/Dotfiles/${VARIABLE}vs${BASELINE}_for_${CONSTANT}r_${PERTURBATION}.dot
-	#git mv $INPUTDIR/${VARIABLE}vs${BASELINE}_for_${CONSTANT}r_${PERTURBATION}.pdf Results/Networks/${VARIABLE}vs${BASELINE}_for_${CONSTANT}r_${PERTURBATION}.pdf
-	git mv Results/Networks/${VARIABLE}vs${BASELINE}_for_${CONSTANT}r_${PERTURBATION}.xlsx  Results/ExcelFiles/${VARIABLE}vs${BASELINE}_for_${CONSTANT}r_${PERTURBATION}.xlsx
+	INPUTDIR=Results/${VARIABLE}vs${BASELINE}_for_${CONSTANT}r_${PERTURBATION}
+	git mv $INPUTDIR/network_solution.dot Results/Dotfiles/${VARIABLE}vs${BASELINE}_for_${CONSTANT}r_${PERTURBATION}.dot
+	git mv $INPUTDIR/${VARIABLE}vs${BASELINE}_for_${CONSTANT}r_${PERTURBATION}.pdf Results/Networks/${VARIABLE}vs${BASELINE}_for_${CONSTANT}r_${PERTURBATION}.pdf
+	#git mv Results/Networks/${VARIABLE}vs${BASELINE}_for_${CONSTANT}r_${PERTURBATION}.xlsx  Results/ExcelFiles/${VARIABLE}vs${BASELINE}_for_${CONSTANT}r_${PERTURBATION}.xlsx
     done
 done
 git status
